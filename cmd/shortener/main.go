3e0b06c3cd84ae9666d4dfee86d94620cb072c26
Panov Alexey <alexeykapanov@gmail.com>
2022-02-07 17:57:46 +0300

    [increment-1] Add blank for http server.

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,38 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"net/http"
+)
+
+const serverPort uint16 = 8080
+
+type MainHandler struct {
+	Template []byte
+	URL      []string
+}
+
+func (h MainHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte(""))
+}
+
+func main() {
+	mainHandler := MainHandler{
+		URL: getInitialURLArray(),
+	}
+	http.ListenAndServe(getServerPort(), mainHandler)
+}
+
+func getInitialURLArray() []string {
+	urlArray := []string{
+		"www.yandex.ru",
+		"www.google.com",
+		"www.codeblog.pro",
+	}
+
+	return urlArray
+}
+
+func getServerPort() string {
+	return ":" + fmt.Sprint(serverPort)
+}
