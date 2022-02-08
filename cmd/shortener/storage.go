abcfef5f951ee6af9abf6bdac61e6e0b2de14b17
Panov Alexey <alexeykapanov@gmail.com>
2022-02-08 11:11:36 +0300

    [increment-2] Separate package main.

---
+++ cmd/shortener/storage.go
@@ -0,0 +1,11 @@
+package main
+
+func getInitialURLMap() map[string]string {
+	urlMap := map[string]string{
+		"yandex": "http://www.yandex.ru",
+		"google": "http://www.google.com",
+		"meta":   "http://about.facebook.com/meta/",
+	}
+
+	return urlMap
+}
