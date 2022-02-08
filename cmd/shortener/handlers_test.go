c500634adcedcbaa175044707f180c0f6235ea2d
Panov Alexey <alexeykapanov@gmail.com>
2022-02-08 06:19:00 +0300

    [increment-2] Add test for getShortURLCode function.

--- cmd/shortener/handlers_test.go
+++ cmd/shortener/handlers_test.go
@@ -33,3 +33,32 @@ func Test_getShortURLByLongURL(t *testing.T) {
 		})
 	}
 }
+
+func Test_getShortURLCode(t *testing.T) {
+	tests := []struct {
+		name  string
+		value string
+		want  string
+	}{
+		{
+			name:  "Test by vk.com service with http scheme",
+			value: "vk.com",
+			want:  "7",
+		},
+		{
+			name:  "Test by mamba service with http scheme",
+			value: "http://mamba.ru",
+			want:  "16",
+		},
+		{
+			name:  "Test by facebook service with https scheme",
+			value: "https://facebook.com",
+			want:  "21",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equal(t, getShortURLCode(tt.value), tt.want)
+		})
+	}
+}
