06114dd9979fabed5426ed3a081fba92cbbf0b93
Panov Alexey <alexeykapanov@gmail.com>
2022-02-08 12:14:18 +0300

    [increment-2] Add test for getShortURLByLongURL function.

---
+++ cmd/shortener/handlers_test.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Test_getShortURLByLongURL(t *testing.T) {
+	tests := []struct {
+		name  string
+		value string
+		want  string
+	}{
+		{
+			name:  "Test by vk.com service with http scheme",
+			value: "vk.com",
+			want:  "http://localhost:8080/7",
+		},
+		{
+			name:  "Test by mamba service with http scheme",
+			value: "http://mamba.ru",
+			want:  "http://localhost:8080/16",
+		},
+		{
+			name:  "Test by facebook service with https scheme",
+			value: "https://facebook.com",
+			want:  "http://localhost:8080/21",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equal(t, getShortURLByLongURL(tt.value), tt.want)
+		})
+	}
+}
