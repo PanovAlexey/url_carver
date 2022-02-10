2d9db4520a26a9b8cd0cb9ed52e76c2203bdff3d
Panov Alexey <alexeykapanov@gmail.com>
2022-02-10 08:32:00 +0300

    [increment-3] Use global variable for URLs list.

--- cmd/shortener/storage.go
+++ cmd/shortener/storage.go
@@ -1,11 +1,39 @@
 package main
 
-func getInitialURLMap() map[string]string {
-	urlMap := map[string]string{
-		"yandex": "http://www.yandex.ru",
-		"google": "http://www.google.com",
-		"meta":   "http://about.facebook.com/meta/",
+type URLs struct {
+	urlMap map[string]string
+}
+
+func (u *URLs) AddEmail(key string, email string) bool {
+	if u.urlMap == nil {
+		u.urlMap = make(map[string]string)
 	}
 
-	return urlMap
+	if u.IsExistEmailByKey(key) {
+		return false
+	}
+
+	u.urlMap[key] = email
+
+	return true
+}
+
+func (u *URLs) GetEmailByKey(key string) string {
+	return u.urlMap[key]
+}
+
+func (u *URLs) IsExistEmailByKey(key string) bool {
+	if _, ok := u.urlMap[key]; ok {
+		return true
+	}
+
+	return false
+}
+
+var globalURLs = URLs{}
+
+func InitialURLMap() {
+	globalURLs.AddEmail("yandex", "http://www.yandex.ru")
+	globalURLs.AddEmail("google", "http://www.google.com")
+	globalURLs.AddEmail("meta", "http://about.facebook.com/meta/")
 }
