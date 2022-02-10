57b9168aa48623a00fc85c4efb036c967a3d4969
Panov Alexey <alexeykapanov@gmail.com>
2022-02-10 08:59:00 +0300

    [increment-3] Use Chi router for handlers.

--- cmd/shortener/server.go
+++ cmd/shortener/server.go
@@ -2,16 +2,24 @@ package main
 
 import (
 	"fmt"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"log"
 	"net/http"
 )
 
 const serverPort uint16 = 8080
 
 func runServer() {
-	mainHandler := MainHandler{
-		URL: getInitialURLMap(),
-	}
-	http.ListenAndServe(getServerPort(), mainHandler)
+	InitialURLMap()
+
+	router := chi.NewRouter()
+	router.Use(middleware.Logger)
+	router.Get("/{id}", handleGetUrl)
+	router.Post("/", handleAddUrl)
+	log.Println("Starting server...")
+	http.ListenAndServe(getServerPort(), router)
+	log.Println("Server stopped.")
 }
 
 func getServerPort() string {
