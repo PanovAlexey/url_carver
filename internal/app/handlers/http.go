26055c77672da9f1799ddd62e619fd1507eed009
Panov Alexey <alexeykapanov@gmail.com>
2022-02-17 10:23:00 +0300

    [refactoring] Use DTO and entity in add url handler.

--- internal/app/handlers/http.go
+++ internal/app/handlers/http.go
@@ -36,6 +36,26 @@ func (h *httpHandler) HandleGetURL(w http.ResponseWriter, r *http.Request) {
 
 func (h *httpHandler) HandleAddURL(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain;charset=utf-8")
+	defer r.Body.Close()
+	body, err := io.ReadAll(r.Body)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	if len(body) == 0 {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	longURLDto := h.shortURLService.CreateLongURLDto()
+	longURLDto.SetValue(string(body))
+	url := h.shortURLService.GetURLByLongURLDto(longURLDto)
+
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte(url.ShortURL))
+}
 
 	defer r.Body.Close()
 	longURL, err := io.ReadAll(r.Body)
