014f836fe3cbe1ee256fa8a21a4b3fb4f3ad071c
Panov Alexey <alexeykapanov@gmail.com>
2022-02-21 08:22:00 +0300

    [increment-5] Load env variables from file.

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -5,11 +5,21 @@ import (
 	"github.com/PanovAlexey/url_carver/internal/app/repositories"
 	"github.com/PanovAlexey/url_carver/internal/app/servers"
 	"github.com/PanovAlexey/url_carver/internal/app/services"
+	"github.com/joho/godotenv"
+	"log"
 )
 
 func main() {
+	initialize()
+
 	emailRepository := repositories.GetEmailRepository()
 	shortURLService := services.GetShortURLService(emailRepository)
 	httpHandler := http.GetHTTPHandler(shortURLService)
 	servers.RunServer(httpHandler)
+}
+
+func initialize() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("error loading env variables: %s", err.Error())
+	}
 }
