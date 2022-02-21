cfd16d0aee0aa6cc9b0cccb4a436b571d64e43f2
Panov Alexey <alexeykapanov@gmail.com>
2022-02-21 08:59:00 +0300

    [increment-5] Use config vie interfaces.

--- config/config.go
+++ config/config.go
@@ -17,11 +17,23 @@ type Config struct {
 func New() *Config {
 	return &Config{
 		Server: ServerConfig{
-			ServerAddress: getEnv("SERVER_ADDRESS", "http://localhost"),
+			ServerAddress: getEnv("SERVER_ADDRESS", "localhost"),
 			ServerPort:    getEnv("SERVER_PORT", "8080"),
 			BaseURL:       getEnv("BASE_URL", "http://localhost"),
 		},
 	}
+}
+
+func (config Config) GetBaseURL() string {
+	return config.Server.BaseURL
+}
+
+func (config Config) GetPort() string {
+	return config.Server.ServerPort
+}
+
+func (config Config) GetServerAddress() string {
+	return config.Server.ServerAddress
 }
 
 func getEnv(key string, defaultValue string) string {
