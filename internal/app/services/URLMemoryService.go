0da8e9227fb68eec9d2c7b93c0112b8dd8b41ae5
Panov Alexey <alexeykapanov@gmail.com>
2022-03-03 13:25:57 +0300

    [increment-7] Shorten service names. Remove prefix URL.

--- internal/app/services/URLMemoryService.go
+++ internal/app/services/URLMemoryService.go
@@ -11,7 +11,7 @@ type repositoryInterface interface {
 	IsExistURLByKey(key string) bool
 }
 
-type URLMemoryServiceConfigInterface interface {
+type memoryServiceConfigInterface interface {
 	GetBaseURL() string
 }
 
@@ -20,47 +20,47 @@ type shorteningServiceInterface interface {
 	GetShortURLCode(longURL string) string
 }
 
-type URLMemoryService struct {
+type memoryService struct {
 	repository        repositoryInterface
-	config            URLMemoryServiceConfigInterface
+	config            memoryServiceConfigInterface
 	shorteningService shorteningServiceInterface
 }
 
-func GetURLMemoryService(
-	config URLMemoryServiceConfigInterface,
+func GetMemoryService(
+	config memoryServiceConfigInterface,
 	repository repositoryInterface,
 	shorteningService shorteningServiceInterface,
-) *URLMemoryService {
-	return &URLMemoryService{config: config, repository: repository, shorteningService: shorteningService}
+) *memoryService {
+	return &memoryService{config: config, repository: repository, shorteningService: shorteningService}
 }
 
-func (service URLMemoryService) GetURLByKey(key string) string {
+func (service memoryService) GetURLByKey(key string) string {
 	return service.repository.GetURLByKey(key)
 }
 
-func (service URLMemoryService) IsExistURLByKey(key string) bool {
+func (service memoryService) IsExistURLByKey(key string) bool {
 	return service.repository.IsExistURLByKey(key)
 }
 
-func (service URLMemoryService) CreateLongURLDto() dto.LongURL {
+func (service memoryService) CreateLongURLDto() dto.LongURL {
 	return dto.GetLongURLByValue("")
 }
 
-func (service URLMemoryService) GetURLByLongURLDto(longURLDto dto.LongURL) url.URL {
+func (service memoryService) GetURLByLongURLDto(longURLDto dto.LongURL) url.URL {
 	return url.New(longURLDto.Value, service.cutAndAddURL(longURLDto.Value))
 }
 
-func (service URLMemoryService) GetShortURLDtoByURL(url url.URL) dto.ShortURL {
+func (service memoryService) GetShortURLDtoByURL(url url.URL) dto.ShortURL {
 	return dto.GetShortURLByValue(service.shorteningService.GetShortURLWithDomain(url.ShortURL))
 }
 
-func (service URLMemoryService) LoadURLs(collection dto.URLCollection) {
+func (service memoryService) LoadURLs(collection dto.URLCollection) {
 	for _, url := range collection.GetCollection() {
 		service.repository.AddURL(url.ShortURL, url.LongURL)
 	}
 }
 
-func (service URLMemoryService) cutAndAddURL(longURL string) string {
+func (service memoryService) cutAndAddURL(longURL string) string {
 	shortURLCode := service.shorteningService.GetShortURLCode(longURL)
 	service.repository.AddURL(shortURLCode, longURL)
 
