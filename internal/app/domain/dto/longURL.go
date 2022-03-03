e4a72495bcf58fe0871abd8d1842e5eae1a23c70
Panov Alexey <alexeykapanov@gmail.com>
2022-03-03 18:49:54 +0300

    [increment-7] Fix for tests.

--- internal/app/domain/dto/longURL.go
+++ internal/app/domain/dto/longURL.go
@@ -2,17 +2,13 @@ package dto
 
 // LongURL - entity to pass the source URL
 type LongURL struct {
-	value string `json:"url"`
+	Value string `json:"url"`
 }
 
 func GetLongURLByValue(value string) LongURL {
-	return LongURL{value: value}
+	return LongURL{Value: value}
 }
 
 func (longUrl *LongURL) SetValue(value string) {
-	longUrl.value = value
-}
-
-func (longUrl *LongURL) GetValue() string {
-	return longUrl.value
+	longUrl.Value = value
 }
