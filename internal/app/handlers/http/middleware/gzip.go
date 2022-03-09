4b20e3a83686c813ecdbc46f328c21877cd2f6a2
Panov Alexey <alexeykapanov@gmail.com>
2022-03-09 11:44:00 +0300

    [increment-8] Decompress request if needed.

--- internal/app/handlers/http/middleware/gzip.go
+++ internal/app/handlers/http/middleware/gzip.go
@@ -18,21 +18,47 @@ func (w gzipWriter) Write(b []byte) (int, error) {
 
 func GZip(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if !strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") {
-			next.ServeHTTP(w, r)
+		if isNeedToDecompressRequest(*r) {
+			decompressRequest(w, r)
+		}
+
+		if isNeedToCompressResponse(*r) {
+			compressResponse(w, r, next)
 			return
 		}
 
-		gz, err := gzip.NewWriterLevel(w, gzip.BestCompression)
-
-		if err != nil {
-			io.WriteString(w, err.Error())
-			return
-		}
-
-		defer gz.Close()
-
-		w.Header().Set("Content-Encoding", "gzip")
-		next.ServeHTTP(gzipWriter{ResponseWriter: w, Writer: gz}, r)
+		next.ServeHTTP(w, r)
+		return
 	})
+}
+
+func isNeedToDecompressRequest(r http.Request) bool {
+	return strings.Contains(r.Header.Get("Content-Encoding"), "application/gzip")
+}
+
+func decompressRequest(w http.ResponseWriter, r *http.Request) {
+	result, err := gzip.NewReader(r.Body)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	r.Body = result
+}
+
+func isNeedToCompressResponse(r http.Request) bool {
+	return strings.Contains(r.Header.Get("Accept-Encoding"), "gzip")
+}
+
+func compressResponse(w http.ResponseWriter, r *http.Request, next http.Handler) {
+	gz, err := gzip.NewWriterLevel(w, gzip.BestCompression)
+	defer gz.Close()
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	w.Header().Set("Content-Encoding", "application/gzip")
+	next.ServeHTTP(gzipWriter{ResponseWriter: w, Writer: gz}, r)
 }
