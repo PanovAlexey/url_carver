2bdecb5a30512e0042b8011dd0028a9c88216174
Panov Alexey <alexeykapanov@gmail.com>
2022-03-12 08:45:00 +0300

    [increment-8] Add tests for gzip compression.

--- internal/app/handlers/http/handlers_test.go
+++ internal/app/handlers/http/handlers_test.go
@@ -2,6 +2,8 @@ package http
 
 import (
 	"bytes"
+	"compress/gzip"
+	"fmt"
 	"github.com/PanovAlexey/url_carver/config"
 	"github.com/PanovAlexey/url_carver/internal/app/repositories"
 	"github.com/PanovAlexey/url_carver/internal/app/services"
@@ -15,10 +17,16 @@ import (
 	"testing"
 )
 
-func testRequest(t *testing.T, ts *httptest.Server, method, path string, body []byte) (*http.Response, string) {
+func testRequest(
+	t *testing.T, ts *httptest.Server, method, path string, body []byte, headers map[string]string,
+) (*http.Response, string) {
 	bodyIoReader := bytes.NewBuffer(body)
 	req, err := http.NewRequest(method, ts.URL+path, bodyIoReader)
 	require.NoError(t, err)
+
+	for key, value := range headers {
+		req.Header.Set(key, value)
+	}
 
 	client := &http.Client{
 		CheckRedirect: func(req *http.Request, via []*http.Request) error {
@@ -49,12 +57,14 @@ func Test_handleAddAndGetRequests(t *testing.T) {
 		response          string
 		locationHeader    string
 		contentTypeHeader string
+		contentEncoding   string
 	}
 
 	tests := []struct {
 		name    string
 		urlPath string
 		method  string
+		headers map[string]string
 		body    []byte
 		want    want
 	}{
@@ -166,10 +176,58 @@ func Test_handleAddAndGetRequests(t *testing.T) {
 				locationHeader:    "https://3dnews.com",
 			},
 		},
+		{
+			name:    "Positive test. Get short url for 3dnews site after it was added with GZIP compression.",
+			urlPath: "/19",
+			method:  http.MethodGet,
+			body:    nil,
+			headers: map[string]string{
+				"Accept-Encoding": "gzip",
+			},
+			want: want{
+				code:              http.StatusTemporaryRedirect,
+				response:          "\x1f\x8b\b\x00\x00\x00\x00\x00\x02\xff\x01\x00\x00\xff\xff\x00\x00\x00\x00\x00\x00\x00\x00",
+				contentTypeHeader: "text/plain;charset=utf-8",
+				locationHeader:    "https://3dnews.com",
+				contentEncoding:   "gzip",
+			},
+		},
+		{
+			name:    "Positive test. Add Pikabu site url with GZIP compression.",
+			urlPath: "/",
+			method:  http.MethodPost,
+			body:    []byte(`http://pikabu.com`),
+			headers: map[string]string{
+				"Accept-Encoding": "gzip",
+			},
+			want: want{
+				code:              http.StatusCreated,
+				response:          "\x1f\x8b\b\x00\x00\x00\x00\x00\x02\xff\xca())\xb0\xd2\xd7\xcf\xc9ON\xcc\xc9\xc8/.\xb1\xb20\xb00\xd07\xb4\x00\x04\x00\x00\xff\xff\xc6@\x82\xbe\x18\x00\x00\x00",
+				contentTypeHeader: "text/plain;charset=utf-8",
+				locationHeader:    "",
+				contentEncoding:   "gzip",
+			},
+		},
+		{
+			name:    "API. Positive test. Add 3dnews site url with GZIP compression.",
+			urlPath: "/api/shorten",
+			method:  http.MethodPost,
+			body:    []byte(`{"url": "https://3dnews.com"}`),
+			headers: map[string]string{
+				"Accept-Encoding": "gzip",
+			},
+			want: want{
+				code:              http.StatusCreated,
+				response:          "\x1f\x8b\b\x00\x00\x00\x00\x00\x02\xff\xaaV*J-.\xcd)Q\xb2R\xca())\xb0\xd2\xd7\xcf\xc9ON\xcc\xc9\xc8/.\xb1\xb20\xb00\xd07\xb4T\xaa\x05\x04\x00\x00\xff\xff\x00V\x8b\xae%\x00\x00\x00",
+				contentTypeHeader: "application/json",
+				locationHeader:    "",
+				contentEncoding:   "gzip",
+			},
+		},
 	}
 
 	for _, testData := range tests {
-		response, bodyString := testRequest(t, server, testData.method, testData.urlPath, testData.body)
+		response, bodyString := testRequest(t, server, testData.method, testData.urlPath, testData.body, testData.headers)
 
 		if response != nil {
 			defer response.Body.Close()
@@ -179,7 +237,31 @@ func Test_handleAddAndGetRequests(t *testing.T) {
 		assert.Equal(t, bodyString, testData.want.response)
 		assert.Equal(t, response.Header.Get("Content-Type"), testData.want.contentTypeHeader)
 		assert.Equal(t, response.Header.Get("location"), testData.want.locationHeader)
+		assert.Equal(t, response.Header.Get("Content-Encoding"), testData.want.contentEncoding)
 	}
+}
+
+func Compress(data []byte) ([]byte, error) {
+	var b bytes.Buffer
+	w, err := gzip.NewWriterLevel(&b, gzip.BestCompression)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed init compress writer: %v", err)
+	}
+
+	_, err = w.Write(data)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed write data to compress temporary buffer: %v", err)
+	}
+
+	err = w.Close()
+
+	if err != nil {
+		return nil, fmt.Errorf("failed compress data: %v", err)
+	}
+
+	return b.Bytes(), nil
 }
 
 func getRouterForRouteTest() chi.Router {
