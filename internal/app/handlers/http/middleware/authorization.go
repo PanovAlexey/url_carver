2a75e3cc9e72c7dedf9457dab1aabb146b3e91aa
Panov Alexey <alexeykapanov@gmail.com>
2022-03-13 13:31:00 +0300

    [increment-9] Implement the generation of a unique token and setting it in the user's cookie.

--- internal/app/handlers/http/middleware/authorization.go
+++ internal/app/handlers/http/middleware/authorization.go
@@ -1,11 +1,59 @@
 package middleware
 
 import (
+	"github.com/google/uuid"
 	"net/http"
+	"time"
 )
+
+const userTokenName = `token`
+const userTokenCookieExpirationDate = 1 * time.Minute
 
 func Authorization(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		userToken := getUserTokenFromCookie(r)
+
+		if !isUserTokenValid(userToken) {
+			userToken = userTokenGenerate()
+			setUserTokenToCookie(userToken, w)
+		}
+
 		next.ServeHTTP(w, r)
 	})
+}
+
+func getUserTokenFromCookie(r *http.Request) string {
+	userTokenCookie, err := r.Cookie(userTokenName)
+
+	if err != nil {
+		if err != http.ErrNoCookie {
+			// @ToDo: log some error oO
+		}
+
+		return ``
+	}
+
+	return (*userTokenCookie).Value
+}
+
+func isUserTokenValid(userToken string) bool {
+	if len(userToken) < 1 {
+		return false
+	}
+
+	return true
+}
+
+func userTokenGenerate() string {
+	return uuid.New().String()
+}
+
+func setUserTokenToCookie(userToken string, w http.ResponseWriter) {
+	cookie := http.Cookie{
+		Name:    userTokenName,
+		Value:   userToken,
+		Expires: time.Now().Add(userTokenCookieExpirationDate),
+	}
+
+	http.SetCookie(w, &cookie)
 }
