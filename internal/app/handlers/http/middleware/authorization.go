7536d8e36fabd16bc96ad84aab4462bdc7d9f6ce
Panov Alexey <alexeykapanov@gmail.com>
2022-03-16 14:44:00 +0300

    [increment-9] Move methods from middleware to service.

--- internal/app/handlers/http/middleware/authorization.go
+++ internal/app/handlers/http/middleware/authorization.go
@@ -3,56 +3,22 @@ package middleware
 import (
 	"github.com/PanovAlexey/url_carver/internal/app/services"
 	"github.com/PanovAlexey/url_carver/internal/app/services/encryption"
-	"log"
 	"net/http"
-	"time"
 )
 
 func Authorization(encryptionService encryption.EncryptorInterface) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			userToken := getUserTokenFromCookie(r, encryptionService)
 			userTokenAuthorizationService := services.GetUserTokenAuthorizationService()
+			userToken := userTokenAuthorizationService.GetUserTokenFromCookie(r, encryptionService)
 
 			if !userTokenAuthorizationService.IsUserTokenValid(userToken) {
 				userToken = userTokenAuthorizationService.UserTokenGenerate()
 				userTokenEncrypted := encryptionService.Encrypt(userToken)
-				setUserTokenToCookie(userTokenEncrypted, w)
+				userTokenAuthorizationService.SetUserTokenToCookie(userTokenEncrypted, w)
 			}
 
 			next.ServeHTTP(w, r)
 		})
 	}
-}
-
-func getUserTokenFromCookie(r *http.Request, encryptionService encryption.EncryptorInterface) string {
-	userToken := ``
-	userTokenCookie, err := r.Cookie(services.UserTokenName)
-
-	if err != nil {
-		if err != http.ErrNoCookie {
-			log.Println("error with getting token from cookie: " + err.Error())
-		}
-
-		return userToken
-	}
-
-	userTokenEncrypted := (*userTokenCookie).Value
-	userToken, err = encryptionService.Decrypt(userTokenEncrypted)
-
-	if err != nil {
-		log.Println("error with decrypting token from cookie: " + err.Error())
-	}
-
-	return userToken
-}
-
-func setUserTokenToCookie(userToken string, w http.ResponseWriter) {
-	cookie := http.Cookie{
-		Name:    services.UserTokenName,
-		Value:   userToken,
-		Expires: time.Now().Add(services.UserTokenCookieExpirationDate),
-	}
-
-	http.SetCookie(w, &cookie)
 }
