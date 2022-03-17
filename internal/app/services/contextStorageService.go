91e7c2c6675fef7e9a4029e7b12c3a1fad9d7088
Panov Alexey <alexeykapanov@gmail.com>
2022-03-17 05:46:00 +0300

    [increment-9] Crutch for autotests.

--- internal/app/services/contextStorageService.go
+++ internal/app/services/contextStorageService.go
@@ -6,7 +6,11 @@ import (
 	"net/http"
 )
 
-const userTokenKey = "token"
+type key string
+
+const (
+	userTokenKey key = "token"
+)
 
 type contextStorageService struct {
 }
@@ -17,6 +21,7 @@ func GetContextStorageService() contextStorageService {
 
 func (service contextStorageService) SaveUserTokenToContext(r http.Request, userToken string) http.Request {
 	ctx := context.WithValue(r.Context(), userTokenKey, userToken)
+
 	return *r.WithContext(ctx)
 }
 
