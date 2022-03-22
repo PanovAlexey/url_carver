984108a2bdff073c8addbd72696b47b276c38ccb
Panov Alexey <alexeykapanov@gmail.com>
2022-03-22 08:33:00 +0300

    [increment-10] Create service for working with URL in database.

---
+++ internal/app/services/URLDatabaseService.go
@@ -0,0 +1,78 @@
+package services
+
+import (
+	"github.com/PanovAlexey/url_carver/internal/app/domain"
+	"github.com/PanovAlexey/url_carver/internal/app/domain/dto"
+	"github.com/PanovAlexey/url_carver/internal/app/domain/entity/url"
+	"github.com/PanovAlexey/url_carver/internal/app/domain/entity/user"
+	"log"
+)
+
+type databaseURLService struct {
+	databaseRepository  databaseURLRepositoryInterface
+	databaseUserService databaseUserService
+}
+
+type databaseURLRepositoryInterface interface {
+	SaveURL(url dto.DatabaseURLInterface) (int, error)
+	GetURLByKey(key string) url.URL
+}
+
+func GetDatabaseURLService(
+	databaseRepository databaseURLRepositoryInterface,
+	databaseUserService databaseUserService,
+) *databaseURLService {
+	return &databaseURLService{
+		databaseRepository:  databaseRepository,
+		databaseUserService: databaseUserService,
+	}
+}
+
+func (service databaseURLService) SaveURL(url domain.URLInterface) (int, error) {
+	var userID int
+
+	if service.databaseUserService.IsExistUserByToken(url.GetUserToken()) {
+		userEntity, err := service.databaseUserService.GetUserByToken(url.GetUserToken())
+		userID = userEntity.GetID()
+
+		if err != nil {
+			log.Println(`error was found while user getting from database: ` + err.Error())
+		}
+	} else {
+		savedUserID, err := service.databaseUserService.SaveUser(user.New(0, url.GetUserToken()))
+
+		if err != nil {
+			log.Println(`error was found while user saving to database: ` + err.Error())
+			return 0, err // ToDo: 0 is a crutch
+		}
+
+		userID = savedUserID
+
+		log.Println(`user `, userID, ` saving to database successfully completed`)
+	}
+
+	databaseURL := dto.NewDatabaseURL(
+		url.GetLongURL(),
+		url.GetShortURL(),
+		userID,
+	)
+
+	log.Println(`try to save to database URL: `, databaseURL)
+	log.Println(databaseURL)
+
+	URLID, err := service.databaseRepository.SaveURL(databaseURL)
+
+	if err != nil {
+		log.Println("url dit not save to database: " + err.Error())
+	}
+
+	return URLID, err
+}
+
+func (service databaseURLService) GetURLByKey(key string) url.URL {
+	return service.databaseRepository.GetURLByKey(key)
+}
+
+func (service databaseURLService) IsExistURLByKey(key string) bool {
+	return true
+}
