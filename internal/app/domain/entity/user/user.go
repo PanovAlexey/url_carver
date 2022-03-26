90ade47367285b5c75696b6eb90e66a274b1a964
Panov Alexey <alexeykapanov@gmail.com>
2022-03-26 11:17:00 +0300

    [increment-10] Create user entity.

---
+++ internal/app/domain/entity/user/user.go
@@ -0,0 +1,25 @@
+package user
+
+type user struct {
+	id   int
+	guid string
+}
+
+func New(id int, guid string) user {
+	return user{
+		id:   id,
+		guid: guid,
+	}
+}
+
+func (user user) GetID() int {
+	return user.id
+}
+
+func (user user) GetGuid() string {
+	return user.guid
+}
+
+func (user *user) SetGuid(value string) {
+	user.guid = value
+}
