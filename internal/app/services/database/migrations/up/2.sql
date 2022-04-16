fc0cf92ef7d83ade3206a6425ca2f60db0b7ffe8
Panov Alexey <alexeykapanov@gmail.com>
2022-04-16 07:11:00 +0300

    [increment-14] Add flag-field to database migration indicating whether the URL has been removed.

--- internal/app/services/database/migrations/up/2.sql
+++ internal/app/services/database/migrations/up/2.sql
@@ -3,6 +3,7 @@ CREATE TABLE IF NOT EXISTS urls (
     user_id integer not null,
     url varchar(255) not null unique,
     short_url varchar(255) not null unique,
+    is_deleted boolean not null DEFAULT false,
     PRIMARY KEY(id),
     CONSTRAINT fk_user
         FOREIGN KEY(user_id)
