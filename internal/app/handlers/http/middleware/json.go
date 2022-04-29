4ac129c325a2dcd31cbb00a98029d442844aa3a2
Panov Alexey <alexeykapanov@gmail.com>
2022-04-29 15:22:00 +0300

    [increment-14] Simplify verification.

--- internal/app/handlers/http/middleware/json.go
+++ internal/app/handlers/http/middleware/json.go
@@ -6,9 +6,7 @@ import (
 
 func JSON(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		requestContentType := r.Header.Get("Content-Type")
-
-		if requestContentType != "application/json" {
+		if r.Header.Get("Content-Type") != "application/json" {
 			http.Error(w, "Invalid Content-Type", http.StatusBadRequest)
 			return
 		}
