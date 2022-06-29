2b4bd677e6a4403bbd32aea22f4b67e74a3dc1f0
Panov Alexey <alexeykapanov@gmail.com>
2022-06-29 20:54:59 +0300

    Use graceful shutdown in case of server shutdown by the signal.
    Fix codestyle for autotests.

--- internal/app/servers/server.go
+++ internal/app/servers/server.go
@@ -2,39 +2,87 @@
 package servers
 
 import (
+	"context"
 	"github.com/PanovAlexey/url_carver/config"
 	"github.com/go-chi/chi/v5"
 	"log"
 	"net/http"
 	_ "net/http/pprof"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 type HandlerInterface interface {
 	NewRouter() chi.Router
 }
 
-func RunServer(handler HandlerInterface, config config.Config) {
-	router := handler.NewRouter()
+type server struct {
+	httpServer *http.Server
+}
 
+func RunServer(handler HandlerInterface, config config.Config) {
+	runDocumentationServer(config)
+	runURLCarverServer(handler, config)
+}
+
+func runDocumentationServer(config config.Config) {
 	if config.IsDebug() {
 		go func() {
+			log.Println("Starting documentation server...")
 			err := http.ListenAndServe(config.GetServerDebugAddress(), nil)
 
 			if err != nil {
 				log.Printf("error occurred while running http documentation server: %s", err.Error())
 			}
+
+			log.Println("Documentation server stopped.")
 		}()
 	}
+}
 
-	log.Println("Starting server...")
+func runURLCarverServer(handler HandlerInterface, config config.Config) {
+	log.Println("UrlCarver starting server...")
+	router := handler.NewRouter()
+	srv := newServer(config.GetServerAddress(), router)
 
-	if config.IsEnableHTTPS() {
-		log.Fatal(
-			http.ListenAndServeTLS(config.GetServerAddress(), "./config/ssl/cert.pem", "./config/ssl/key.pem", router),
-		)
-	} else {
-		log.Fatal(http.ListenAndServe(config.GetServerAddress(), router))
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		if config.IsEnableHTTPS() {
+			log.Println(
+				srv.httpServer.ListenAndServeTLS("./config/ssl/cert.pem", "./config/ssl/key.pem"),
+			)
+		} else {
+			log.Println(srv.httpServer.ListenAndServe())
+		}
+	}()
+
+	log.Println("Signal detected: ", <-sigs)
+
+	ctx, cancel := context.WithTimeout(
+		context.Background(), time.Duration(config.Server.TimeoutShutdownInSeconds)*time.Second,
+	)
+	defer cancel()
+
+	err := srv.httpServer.Shutdown(ctx)
+	log.Println("UrlCarver server is shutdowning...")
+
+	if err != nil {
+		log.Println(err)
 	}
 
-	log.Println("Server stopped.")
+	log.Println("UrlCarver server stopped.")
+}
+
+func newServer(address string, handler http.Handler) server {
+	srv := new(server)
+	srv.httpServer = &http.Server{
+		Addr:    address,
+		Handler: handler,
+	}
+
+	return *srv
 }
