76fd68c158804bb2acdf8e79b0ec4868ac5f27b4
Panov Alexey <alexeykapanov@gmail.com>
2022-07-12 18:30:51 +0300

    Replace hardcode trusted sub net value to config value.

--- internal/app/handlers/http/middleware/authorizationByIP.go
+++ internal/app/handlers/http/middleware/authorizationByIP.go
@@ -1,20 +1,23 @@
 package middleware
 
 import (
+	"github.com/PanovAlexey/url_carver/config"
 	"net"
 	"net/http"
 )
 
 func AuthorizationByIP(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		_, IPNet, err := net.ParseCIDR("127.0.0.1")
+		trustedSubNet := (config.New()).GetTrustedSubNet()
+
+		_, IPNet, err := net.ParseCIDR(trustedSubNet)
 
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusForbidden)
 			return
 		}
 
-		if !IPNet.Contains(net.ParseIP(r.Header.Get("X-Real-IP"))) {
+		if len(trustedSubNet) == 0 || !IPNet.Contains(net.ParseIP(r.Header.Get("X-Real-IP"))) {
 			http.Error(w, "Your subnet is forbidden", http.StatusForbidden)
 			return
 		}
