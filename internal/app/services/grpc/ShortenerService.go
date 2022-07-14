42d027bef012f7042cae3d93427cf991369d6865
Panov Alexey <alexeykapanov@gmail.com>
2022-07-14 19:32:43 +0300

    Update the method of adding a new URL.

--- internal/app/services/grpc/ShortenerService.go
+++ internal/app/services/grpc/ShortenerService.go
@@ -2,18 +2,91 @@ package grpc
 
 import (
 	"context"
+	"fmt"
+	"github.com/PanovAlexey/url_carver/internal/app/services"
+	"github.com/PanovAlexey/url_carver/internal/app/services/database"
 	pb "github.com/PanovAlexey/url_carver/pkg/shortener_grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/types/known/emptypb"
 )
 
 type ShortenerService struct {
 	pb.UnimplementedShortenerServer
+
+	errorService                  services.ErrorService
+	memoryService                 services.MemoryService
+	storageService                services.StorageService
+	shorteningService             services.ShorteningService
+	contextStorageService         services.ContextStorageService
+	userTokenAuthorizationService services.UserTokenAuthorizationService
+	URLMappingService             services.MappingService
+	databaseService               database.DatabaseInterface
+	databaseURLService            services.DatabaseURLService
+	databaseUserService           services.DatabaseUserService
 }
 
-func (s ShortenerService) AddURL(ctx context.Context, in *pb.AddURLRequest) (*pb.AddURLResponse, error) {
+func GetGRPCShortenerService(
+	errorService services.ErrorService,
+	memoryService services.MemoryService,
+	storageService services.StorageService,
+	shorteningService services.ShorteningService,
+	contextStorageService services.ContextStorageService,
+	userTokenAuthorizationService services.UserTokenAuthorizationService,
+	databaseService database.DatabaseInterface,
+	databaseURLService services.DatabaseURLService,
+	databaseUserService services.DatabaseUserService,
+) ShortenerService {
+	return ShortenerService{
+		errorService:                  errorService,
+		memoryService:                 memoryService,
+		storageService:                storageService,
+		shorteningService:             shorteningService,
+		contextStorageService:         contextStorageService,
+		userTokenAuthorizationService: userTokenAuthorizationService,
+		URLMappingService:             services.MappingService{},
+		databaseService:               databaseService,
+		databaseURLService:            databaseURLService,
+		databaseUserService:           databaseUserService,
+	}
+}
+
+func (s ShortenerService) AddURL(ctx context.Context, request *pb.AddURLRequest) (*pb.AddURLResponse, error) {
 	var response pb.AddURLResponse
 	response.ShortURL = "shortURLMock"
 	response.Error = ""
+
+	if len(request.LongURL) == 0 {
+		response.Error = "URL is empty"
+		return &response, status.Errorf(codes.InvalidArgument, response.Error, request.LongURL)
+	}
+
+	url, err := s.shorteningService.GetURLEntityByLongURL(request.LongURL)
+
+	if err != nil {
+		response.Error = err.Error()
+		return &response, status.Errorf(codes.InvalidArgument, err.Error(), request.LongURL)
+	}
+
+	url.UserID = s.contextStorageService.GetUserTokenFromContext(ctx)
+	s.memoryService.SaveURL(url)
+	s.storageService.SaveURL(url)
+	_, err = s.databaseURLService.SaveURL(url)
+
+	if err != nil || len(url.LongURL) == 0 {
+		response.Error = err.Error()
+
+		if s.errorService.IsKeyDuplicated(err) {
+			return &response, status.Errorf(codes.AlreadyExists, err.Error(), request.LongURL)
+		}
+
+		return &response, status.Errorf(codes.Unknown, err.Error(), request.LongURL)
+	}
+
+	shortURLJSON := s.memoryService.GetShortURLDtoByURL(url)
+
+	fmt.Println("URL " + url.LongURL + " added by " + url.ShortURL)
+	response.ShortURL = shortURLJSON.Value
 
 	return &response, nil
 }
