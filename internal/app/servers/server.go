c97d04ed2278d405b6ab659e419a77c1919e3416
Panov Alexey <alexeykapanov@gmail.com>
2022-07-15 22:31:58 +0300

    Run grpc server.

--- internal/app/servers/server.go
+++ internal/app/servers/server.go
@@ -4,8 +4,13 @@ package servers
 import (
 	"context"
 	"github.com/PanovAlexey/url_carver/config"
+	grpcServices "github.com/PanovAlexey/url_carver/internal/app/services/grpc"
+	"github.com/PanovAlexey/url_carver/internal/app/services/grpc/interceptors"
+	pb "github.com/PanovAlexey/url_carver/pkg/shortener_grpc"
 	"github.com/go-chi/chi/v5"
+	"google.golang.org/grpc"
 	"log"
+	"net"
 	"net/http"
 	_ "net/http/pprof"
 	"os"
@@ -18,35 +23,41 @@ type HandlerInterface interface {
 	NewRouter() chi.Router
 }
 
-type server struct {
+type mainServer struct {
 	httpServer *http.Server
 }
 
-func RunServer(handler HandlerInterface, config config.Config) {
-	runDocumentationServer(config)
+type grpcServer struct {
+	server *grpc.Server
+}
+
+func RunServer(handler HandlerInterface, grpcService grpcServices.ShortenerService, config config.Config) {
+	go runDocumentationServer(config)
+	go runURLCarverGRpcServer(grpcService, config)
 	runURLCarverServer(handler, config)
 }
 
 func runDocumentationServer(config config.Config) {
 	if config.IsDebug() {
-		go func() {
-			log.Println("Starting documentation server...")
-			err := http.ListenAndServe(config.GetServerDebugAddress(), nil)
+		log.Println("Documentation starting server...")
+		err := http.ListenAndServe(config.GetServerDebugAddress(), nil)
 
-			if err != nil {
-				log.Printf("error occurred while running http documentation server: %s", err.Error())
-			}
+		if err != nil {
+			log.Printf("error occurred while running http documentation server: %s", err.Error())
+		}
 
-			log.Println("Documentation server stopped.")
-		}()
+		log.Println("Documentation server stopped.")
 	}
 }
 
 func runURLCarverServer(handler HandlerInterface, config config.Config) {
 	log.Println("UrlCarver starting server...")
 	router := handler.NewRouter()
-	srv := newServer(config.GetServerAddress(), router)
-
+	srv := new(mainServer)
+	srv.httpServer = &http.Server{
+		Addr:    config.GetServerAddress(),
+		Handler: router,
+	}
 	sigs := make(chan os.Signal, 1)
 	signal.Notify(sigs, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
 
@@ -65,6 +76,7 @@ func runURLCarverServer(handler HandlerInterface, config config.Config) {
 	ctx, cancel := context.WithTimeout(
 		context.Background(), time.Duration(config.Server.TimeoutShutdownInSeconds)*time.Second,
 	)
+
 	defer cancel()
 
 	err := srv.httpServer.Shutdown(ctx)
@@ -77,12 +89,36 @@ func runURLCarverServer(handler HandlerInterface, config config.Config) {
 	log.Println("UrlCarver server stopped.")
 }
 
-func newServer(address string, handler http.Handler) server {
-	srv := new(server)
-	srv.httpServer = &http.Server{
-		Addr:    address,
-		Handler: handler,
+func runURLCarverGRpcServer(grpcService grpcServices.ShortenerService, config config.Config) {
+	log.Println("UrlCarver gRPC starting server...")
+	listen, err := net.Listen("tcp", ":"+config.GetGRpcServerPort())
+
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	return *srv
+	grpcServer := new(grpcServer)
+	grpcServer.server = grpc.NewServer(grpc.UnaryInterceptor(interceptors.AuthorizationByToken))
+	pb.RegisterShortenerServer(grpcServer.server, &grpcService)
+
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		if err := grpcServer.server.Serve(listen); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	log.Println("Signal detected: ", <-sigs)
+
+	grpcServer.server.GracefulStop()
+	log.Println("UrlCarver gRPC server is shutdowning...")
+
+	if err != nil {
+		log.Println(err)
+	}
+
+	log.Println("UrlCarver gRPC server stopped.")
+
 }
