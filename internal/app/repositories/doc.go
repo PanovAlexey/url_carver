24273c2b22bf4fd18792b0eb00952e5a0f0d57ad
Panov Alexey <alexeykapanov@gmail.com>
2022-08-18 07:22:00 +0300

    Create doc for repositories package.

---
+++ internal/app/repositories/doc.go
@@ -0,0 +1,5 @@
+/*
+Package repositories is a set of application interfaces that must be implemented
+by the infrastructure repositories to work with the main structures of the project
+*/
+package repositories
